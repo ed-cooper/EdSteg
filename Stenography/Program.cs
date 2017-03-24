262f6b2e3f6ad15a60343e4ec86d03f6106d448a
chooper100 <chooper100.scratch@gmail.com>
2017-03-24 14:45:48 +0000

    Add testing code

--- Stenography/Program.cs
+++ Stenography/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Stenography.Encryption;
+using System.Text;
 
 namespace Stenography
 {
@@ -14,9 +17,16 @@ namespace Stenography
         [STAThread]
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Console.Write("Enter plain text: ");
+            string plain = Console.ReadLine();
+            Console.Write("Enter key: ");
+            string key = Console.ReadLine();
+            IEncryptionProvider crypt = new XCrypt(Encoding.Default.GetBytes(key));
+            byte[] cipher = crypt.Encrypt(Encoding.Default.GetBytes(plain));
+            Console.WriteLine("Encrypted: " + Encoding.Default.GetString(cipher));
+            Console.WriteLine("Decrypted: " + Encoding.Default.GetString(crypt.Decrypt(cipher)));
+            Console.Read();
+
         }
     }
 }
