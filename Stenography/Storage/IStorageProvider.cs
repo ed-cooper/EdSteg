d9bf2575e2c4c4cc79b16daa8e1f25f00aac65dd
chooper100 <chooper100.scratch@gmail.com>
2017-03-24 19:02:01 +0000

    Add IStorageProvider

---
+++ Stenography/Storage/IStorageProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace Stenography.Storage
+{
+    interface IStorageProvider
+    {
+        /// <summary>
+        /// Saves the specified data to the specified file.
+        /// </summary>
+        /// <param name="path">The file to save the data to.</param>
+        /// <param name="data">The data to save.</param>
+        void Save(string path, byte[] data);
+
+        /// <summary>
+        /// Reads the data from the specified file.
+        /// </summary>
+        /// <param name="path">The file to read the data from.</param>
+        /// <returns>The data from the speciied file.</returns>
+        byte[] Read(string path);
+    }
+}
