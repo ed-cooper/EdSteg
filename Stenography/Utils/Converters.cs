1d50cc87aecc34c619ee2f571f0c5b09e34e6ef3
chooper100 <chooper100.scratch@gmail.com>
2017-03-24 21:34:25 +0000

    Add ToByteArray extension method

---
+++ Stenography/Utils/Converters.cs
@@ -0,0 +1,27 @@
+﻿namespace Stenography.Utils
+{
+    static class Converters
+    {
+        /// <summary>
+        /// Converts the specified boolean array into a byte array, where each boolean is 1 bit.
+        /// </summary>
+        /// <param name="data">The array to convert into a byte array.</param>
+        /// <returns>
+        /// The byte array containing all the boolean values as bits.
+        /// </returns>
+        public static byte[] ToByteArray(this bool[] data)
+        {
+            int len = data.Length;
+            int bytes = len >> 3;
+            if ((len & 0x07) != 0) ++bytes;
+            byte[] arr2 = new byte[bytes];
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i])
+                    arr2[i >> 3] |= (byte)(1 << (i & 0x07));
+            }
+
+            return arr2;
+        }
+    }
+}
