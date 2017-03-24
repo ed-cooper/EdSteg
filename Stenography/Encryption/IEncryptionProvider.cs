6c2604d937111ae8dd1a75a675c5d0bd3d4038ab
chooper100 <chooper100.scratch@gmail.com>
2017-03-24 14:05:42 +0000

    Add IEncryptionProvider

---
+++ Stenography/Encryption/IEncryptionProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace Stenography.Encryption
+{
+    interface IEncryptionProvider
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the key used for encryption.
+        /// </summary>
+        string Key { get; set; }
+        #endregion
+        #region Functions
+        /// <summary>
+        /// Encrypts the specified plain text.
+        /// </summary>
+        /// <param name="plainText">The plain text to encrypt.</param>
+        /// <returns>The encrypted version of the plain text.</returns>
+        byte[] Encrypt(byte[] plainText);
+
+        /// <summary>
+        /// Decrypts the specified cipher text.
+        /// </summary>
+        /// <param name="cipherText">The cipher text to decrypt.</param>
+        /// <returns>The original plain text.</returns>
+        string Decrypt(byte[] cipherText);
+        #endregion
+    }
+}
