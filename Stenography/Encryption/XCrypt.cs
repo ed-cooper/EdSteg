a70dc8438135d4f80e467e8a8a90b75be996a60f
chooper100 <chooper100.scratch@gmail.com>
2017-03-24 14:12:18 +0000

    Add comments

--- Stenography/Encryption/XCrypt.cs
+++ Stenography/Encryption/XCrypt.cs
@@ -9,21 +9,37 @@ namespace Stenography.Encryption
     public class XCrypt : IEncryptionProvider
     {
         #region Properties
+        /// <summary>
+        /// Gets or sets the key used for encryption.
+        /// </summary>
         public byte[] Key { get; set; }
         #endregion
         #region Constructor
+        /// <summary>
+        /// Creates a new instance of the <see cref="XCrypt"/> class. 
+        /// </summary>
+        /// <param name="key">The key to use for encryption.</param>
         public XCrypt(byte[] key)
         {
             Key = key;
         }
         #endregion
         #region Methods
-
+        /// <summary>
+        /// Encrypts the specified plain text.
+        /// </summary>
+        /// <param name="plainText">The plain text to encrypt.</param>
+        /// <returns>The encrypted version of the plain text.</returns>
         public byte[] Encrypt(byte[] plainText)
         {
 
         }
 
+        /// <summary>
+        /// Decrypts the specified cipher text.
+        /// </summary>
+        /// <param name="cipherText">The cipher text to decrypt.</param>
+        /// <returns>The original plain text.</returns>
         public string Decrypt(byte[] cipherText)
         {
 
