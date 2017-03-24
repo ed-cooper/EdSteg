ebc839949ee0d7925c14062b5b5393cec286eb7f
chooper100 <chooper100.scratch@gmail.com>
2017-03-24 14:10:43 +0000

    Add XCrypt class

---
+++ Stenography/Encryption/XCrypt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stenography.Encryption
+{
+    public class XCrypt : IEncryptionProvider
+    {
+        #region Properties
+        public string Key { get; set; }
+        #endregion
+        #region Constructor
+        public XCrypt(string key)
+        {
+            Key = key;
+        }
+        #endregion
+        #region Methods
+
+        public byte[] Encrypt(byte[] plainText)
+        {
+
+        }
+
+        public string Decrypt(byte[] cipherText)
+        {
+
+        }
+    }
+}
