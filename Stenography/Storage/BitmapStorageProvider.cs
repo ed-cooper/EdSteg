d6dbc9e92b9e5ac6303415b0155d6d755cea7c5f
chooper100 <chooper100.scratch@gmail.com>
2017-03-24 19:05:20 +0000

    Add BitmapStorageProvider

---
+++ Stenography/Storage/BitmapStorageProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace Stenography.Storage
+{
+    public class BitmapStorageProvider : IStorageProvider
+    {
+        /// <summary>
+        /// Saves the specified data to the specified file.
+        /// </summary>
+        /// <param name="path">The file to save the data to.</param>
+        /// <param name="data">The data to save.</param>
+        public unsafe void Save(string path, byte[] data)
+        {
+
+        }
+
+        /// <summary>
+        /// Reads the data from the specified file.
+        /// </summary>
+        /// <param name="path">The file to read the data from.</param>
+        /// <returns>The data from the speciied file.</returns>
+        public unsafe byte[] Read(string path)
+        {
+
+        }
+    }
+}
