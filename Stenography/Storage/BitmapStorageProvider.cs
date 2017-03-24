d827ef4ad25f7f0c580a93133c39269f07152897
chooper100 <chooper100.scratch@gmail.com>
2017-03-24 21:23:21 +0000

    Read length header

--- Stenography/Storage/BitmapStorageProvider.cs
+++ Stenography/Storage/BitmapStorageProvider.cs
@@ -165,8 +165,58 @@ namespace Stenography.Storage
             // Unlock memory
             image.UnlockBits(bmpData);
 
+            // Process data
+
+            // Convert bool data to byte array
+            byte[] byteData = ConvertToByteArray(rawData.ToArray());
+
+            int length;
+            if (BitConverter.IsLittleEndian)
+            {
+                length = BitConverter.ToInt32(new byte[4] {
+                    byteData[3],
+                    byteData[2],
+                    byteData[1],
+                    byteData[0]
+                }, 0);
+            }
+            else
+            {
+                length = BitConverter.ToInt32(byteData, 0);
+            }
+            
             // Return data
             return null;
+        }
+
+        /// <summary>
+        /// Bit-packs an array of booleans into bytes, one bit per boolean.
+        /// </summary>
+        /// <param name="data">An array of booleans to pack into bytes.</param>
+        /// <returns>
+        /// An IEnumerable&lt;byte&gt; of bytes each containing (up to) eight
+        /// bit-packed booleans.
+        /// </returns>
+        /// <remarks>
+        /// Booleans are bit-packed into bytes, in order, from least significant
+        /// bit to most significant bit of each byte.<br/>
+        /// If the length of the input array isn't a multiple of eight, then one
+        /// or more of the most significant bits in the last byte returned will
+        /// be unused. Unused bits are zero / unset.
+        /// </remarks>
+        byte[] ConvertToByteArray(bool[] data)
+        {
+            int len = data.Length;
+            int bytes = len >> 3;
+            if ((len & 0x07) != 0) ++bytes;
+            byte[] arr2 = new byte[bytes];
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i])
+                    arr2[i >> 3] |= (byte)(1 << (i & 0x07));
+            }
+
+            return arr2;
         }
         #endregion
     }
