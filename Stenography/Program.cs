42cc9aa684dd7178e23f93d1f4fdede6d3b863e4
chooper100 <chooper100.scratch@gmail.com>
2017-03-25 00:47:00 +0000

    Change to full winforms app

--- Stenography/Program.cs
+++ Stenography/Program.cs
@@ -18,33 +18,6 @@ namespace Stenography
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new StartupForm());
-            Console.Write("Enter plain text: ");
-            string plain = Console.ReadLine();
-            Console.Write("Enter key: ");
-            string key = Console.ReadLine();
-            IEncryptionProvider crypt = new XCrypt(Encoding.Default.GetBytes(key));
-            byte[] cipher = crypt.Encrypt(Encoding.Default.GetBytes(plain));
-            Console.WriteLine("Encrypted: " + Encoding.Default.GetString(cipher));
-            OpenFileDialog openFile = new OpenFileDialog();
-            if (openFile.ShowDialog() == DialogResult.OK)
-            {
-                Console.WriteLine("File selected: " + openFile.FileName);
-
-                IStorageProvider storageProvider = new BitmapStorageProvider();
-
-                storageProvider.Save(openFile.FileName, "test.png", cipher);
-                Console.WriteLine("File saved");
-                Console.WriteLine("Reading file");
-                cipher = storageProvider.Read("test.png");
-                Console.WriteLine("Read cipher text: " + Encoding.Default.GetString(cipher));
-                Console.WriteLine("Decrypted: " + Encoding.Default.GetString(crypt.Decrypt(cipher)));
-            }
-            else
-            {
-                Console.WriteLine("Operation cancelled");
-            }
-
-            Console.Read();
         }
     }
 }
