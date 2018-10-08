390e7ebea2555484538ce7119f8b56660465cdab
Edward Cooper <edward.cooper100@gmail.com>
2018-10-08 04:11:45 +0100

    Update Assembly Info

--- Stenography/Properties/AssemblyInfo.cs
+++ Stenography/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Stenography")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Ed Steg")]
+[assembly: AssemblyDescription("An educational stenography project to hide messages within images")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stenography")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Ed Steg")]
+[assembly: AssemblyCopyright("Copyright © Edward Cooper 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
