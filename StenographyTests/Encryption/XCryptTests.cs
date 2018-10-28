7b83d2aecec6189622aa65132bf54cf0d80d0a66
Edward Cooper <edward.cooper100@gmail.com>
2018-10-28 16:37:54 +0000

    Create initial unit test

---
+++ StenographyTests/Encryption/XCryptTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stenography.Encryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stenography.Encryption.Tests
+{
+    [TestClass()]
+    public class XCryptTests
+    {
+        [TestMethod()]
+        public void XCryptTest()
+        {
+            byte[] key = Encoding.UTF8.GetBytes("12345");
+            XCrypt crypt = new XCrypt(key);
+
+            Assert.AreEqual(crypt.Key, key);
+        }
+    }
+}
