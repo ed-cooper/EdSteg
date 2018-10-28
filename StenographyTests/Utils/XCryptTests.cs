93b361355c7f4d26b8fc17b553a14a48d5ff72d3
Edward Cooper <edward.cooper100@gmail.com>
2018-10-28 23:50:14 +0000

    Add SetBit tests

---
+++ StenographyTests/Utils/XCryptTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace Stenography.Utils.Tests
+{
+    [TestFixture]
+    public class BinaryUtilsTests
+    {
+        [Test]
+        public void SetBitTest_High()
+        {
+            const byte original = 0b00001000;
+            const byte expected = 0b00001100;
+
+            Assert.AreEqual(BinaryUtils.SetBit(original, 2, true), expected);
+        }
+
+        [Test]
+        public void SetBitTest_Low()
+        {
+            const byte original = 0b11110111;
+            const byte expected = 0b11110011;
+
+            Assert.AreEqual(BinaryUtils.SetBit(original, 2, false), expected);
+        }
+    }
+}
