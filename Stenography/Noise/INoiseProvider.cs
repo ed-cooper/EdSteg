9fbeb387aef2a1f8ede95c1652cd777e11cc282f
Edward Cooper <edward.cooper100@gmail.com>
2018-10-29 09:42:19 +0000

    Create INoiseProvider interface

---
+++ Stenography/Noise/INoiseProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace Stenography.Noise
+{
+    /// <summary>
+    /// Provides noise for disguising the end of the data in an image.
+    /// </summary>
+    public interface INoiseProvider
+    {
+        #region Methods
+
+        /// <summary>
+        /// Gets the next bit of noise.
+        /// </summary>
+        /// <returns>The next bit of noise.</returns>
+        bool Next();
+
+        #endregion
+    }
+}
